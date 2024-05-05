eb8c7b6b77d02dc31b6c4e525a5ae782508a2d47
WYBG <yohan.gagre@yahoo.com>
2024-05-05 17:47:06 +0200

    Mise à jour des codes: Generateur, Pokemon, Classes

--- CSRC_Yohan/Generateur_POKEMON.py
+++ CSRC_Yohan/Generateur_POKEMON.py
@@ -8,8 +8,8 @@ Created on Fri May  3 11:59:02 2024
 import csv
 
 # modèle de pokemon
-modele=("class {nom} (Pokemon):\n"+"    def __init__(self, nom='{nom}', type1='{type1}', type2='{type2}', Total={Total},HP={HP}, Attack={Attack}, Defense={Defense}, Sp_Atk={Sp_Atk}, Sp_Def={Sp_Def}, Speed={Speed}, Generation='{Generation}', Legendary={Legendary}):\n"
-        +"        super().__init__( nom, type1, type2, Total, HP, Attack, Defense , Sp_Atk , Sp_Def , Speed , Generation , Legendary )\n\n")
+modele=("class {nom} (Pokemon):\n"+"    def __init__(self, ID ='{ID}',nom='{nom}', type1='{type1}', type2='{type2}', Total={Total},HP={HP}, Attack={Attack}, Defense={Defense}, Sp_Atk={Sp_Atk}, Sp_Def={Sp_Def}, Speed={Speed}, Generation='{Generation}', Legendary={Legendary}):\n"
+        +"        super().__init__( ID, nom, type1, type2, Total, HP, Attack, Defense , Sp_Atk , Sp_Def , Speed , Generation , Legendary )\n\n")
 
 
 def generateur_pokemon(fichier):
@@ -23,7 +23,9 @@ def generateur_pokemon(fichier):
         for ligne in csvfile:
             nom_pokemon=ligne[1]
             nom_pokemon = nom_pokemon.replace("h'd","hd").replace("Nidoranâ™€","Nidoran_male").replace(". ","_").replace("Nidoranâ™‚","Nidoranâ™").replace("Nidoranâ™","Nidoran_female")
-            Classe= modele.format( nom=nom_pokemon, type1=ligne[2], type2=ligne[3], Total=ligne[4], HP=ligne[5], Attack=ligne[6], Defense=ligne[7], Sp_Atk=ligne[8], Sp_Def=ligne[9],Speed=ligne[10] , Generation=ligne[11] , Legendary=ligne[12])
+            
+            Classe= modele.format( ID = ligne[0], nom=nom_pokemon, type1=ligne[2], type2=ligne[3], Total=ligne[4], HP=ligne[5], Attack=ligne[6], Defense=ligne[7], Sp_Atk=ligne[8], Sp_Def=ligne[9],Speed=ligne[10] , Generation=ligne[11] , Legendary=ligne[12])
+            
             dict_pokemons+="'"+nom_pokemon+"' : "+nom_pokemon+"(), "
             #Fait en sorte que l'absence d'information soit retourné par null et non par le string None
             Classe = Classe.replace("''","'null'")
@@ -41,9 +43,10 @@ if __name__=='__main__':
     #Création du fichier sql de sortie
     Fichier_Classes_Pokemons = open("Pokemons.py", "w")
     #creation de la classe mere
-    Classe_mere=("from abc import abstractmethod, ABCMeta \nimport numpy as np \n\n"+"class Pokemon(metaclass=ABCMeta):\n\n"
+    Classe_mere=("from abc import ABCMeta \nimport numpy as np \n\n"+"class Pokemon(metaclass=ABCMeta):\n\n"
                    +"    Types="+str(Types)+"\n\n    Affinites = np.genfromtxt ('../data/Donnees_crees/Affinites.csv', delimiter =';',skip_header=1,usecols=range(1,19))\n"
-                   +"\n    def __init__(self, nom, type1, type2, Total, HP, Attack, Defense , Sp_Atk , Sp_Def , Speed , Generation , Legendary ):\n"
+                   +"\n    def __init__(self, ID, nom, type1, type2, Total, HP, Attack, Defense , Sp_Atk , Sp_Def , Speed , Generation , Legendary ):\n"
+                   +"        self.ID=ID\n"
                    +"        self.nom=nom\n"
                    +"        self.type1=type1 \n"
                    +"        self.type2=type2 \n"
